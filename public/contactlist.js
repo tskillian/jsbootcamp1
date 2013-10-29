5e24ac5d46ea9cb98fd5213a2b899fa30ff3bbfb
Timothy Killian <tskillian@Timothys-MacBook-Pro.local>
2013-10-29 10:23:04 -0700

    added bower

--- public/contactlist.js
+++ public/contactlist.js
@@ -1,5 +1,4 @@
 
-
 function makeContactRowClickable() {
   $(".table > tbody > tr").click(function() {
     var newUrl = window.location.href;
@@ -10,3 +9,26 @@ function makeContactRowClickable() {
     window.location.href = newUrl;
   });
 };
+
+
+
+
+function sortTable() {
+	var tbl = $('#table');
+}
+
+
+$(document).ready(function() {
+	toSort = []
+	data = $('.last_name');
+	for (var i = 0; i < data.length; i++) {
+		toSort.push($(data[i]).text());
+	sorted = toSort.sort();
+	}
+	tbl = $('tbody');
+    	makeContactRowClickable();
+    $('th').on('click', function() {
+    	alert('work');
+    });
+});
+
